88ef0f9c9e673c6d9361639671df0446dbac3e2e
Victor Chao <chia-chi.chao@tufts.edu>
2014-02-25 16:53:01 -0500

    added dirt and bushes

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,18 @@
-// Your work goes here...
+function drawGameBG()
+{
+        var canvas = document.getElementById('game');
+        if (canvas.getContext) {
+                ctx = canvas.getContext('2d');
+                ctx.fillStyle = "#87CEEB";
+                ctx.fillRect(0, 0, 800, 600);
+                image = new Image();
+                image.onload = function() {
+                        ctx.drawImage(image,
+                                        0, 720, 900, 180, 0, 350, 800, 180);
+                }
+                image.src = 'assets/duckhunt.png';
+        }
+        else {
+                alert('Sorry, canvas is not supported on your browser!');
+        }
+}
