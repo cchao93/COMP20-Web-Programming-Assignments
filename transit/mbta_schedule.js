7dc58e00b3caf2290037c80c35e643270a94e2b0
Victor Chao <chia-chi.chao@tufts.edu>
2014-03-11 14:16:57 -0400

    fixed map more

--- transit/mbta_schedule.js
+++ transit/mbta_schedule.js
@@ -1,6 +1,6 @@
 function init_map() {
         var mapOptions = {
-                center: new google.map.Latlng(42.3581, 71.0636),
+                center: new google.maps.Latlng(42.3581, 71.0636),
                 zoom: 8
         };
         var map = new google.maps.Map(
