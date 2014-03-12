a0d58c23134da02d4bd8a3fe3906c698b8a5d0b8
Victor Chao <chia-chi.chao@tufts.edu>
2014-03-12 02:01:29 -0400

    trying to fix map

--- transit/mbta_schedule.js
+++ transit/mbta_schedule.js
@@ -1,7 +1,7 @@
 function init_map() {
         var mapOptions = {
                 center: new google.maps.LatLng(42.3581, 71.0636),
-                zoom: 8
+                zoom: 10
         };
         var map = new google.maps.Map(
                 document.getElementById("map-canvas"), mapOptions);
